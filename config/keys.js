0665bd9aa7ce8d62c66ea6e7019c8cf36596a879
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-04-20 11:45:16 -0500

    added config keys and code to connect to MongoDB with mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://prakhar:prakhar@ds251799.mlab.com:51799/trainerconnect"
+};
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // initialize app to express
 const app = express();
+
+// db config
+const db = require("./config/keys").mongoURI;
+
+// connecting to MongoDB through Mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // initial route to home page
 app.get("/", (req, res) => res.send("Hello!!!"));
